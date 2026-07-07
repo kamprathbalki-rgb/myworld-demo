6aecfa289e55651c33ffd420391e10d88953c398
kbalki69 <kbalki2k15@gmail.com>
2026-07-07 12:34:12 +0530

    Add debug users route

--- config/db.js
+++ config/db.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
 
-        await mongoose.connect(process.env.DB_URI)
+        await mongoose.connect(process.env.DB_URI_NEW)
+
+        console.log("MongoDB Connected")
 
     } catch (error) {
 
-        console.error("DB_URI =", process.env.DB_URI)
+        console.error("DB_URI_NEW =", process.env.DB_URI_NEW)
         console.error("MongoDB Connection Error:", error)
 
         process.exit(1)
