e075312a6e2aebd7e15bf2ce9c8fc22a88d206b5
kbalki69 <kbalki2k15@gmail.com>
2026-07-07 11:59:17 +0530

    Add debug users route

--- config/db.js
+++ config/db.js
@@ -3,11 +3,9 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
 
-        console.log("DB_URI =", process.env.DB_URI)
-
-        await mongoose.connect(process.env.DB_URI)
-
-        console.log("MongoDB Connected")
+        await mongoose.connect(process.env.DB_URI, {
+    dbName: 'myworld'
+})
 
     } catch (error) {
 
